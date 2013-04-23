2a86351c7da0b30a7af9e3a91c151e456762911a
TReische <treische@wesco.com>
2013-04-23 11:42:22 -0400

    Added batch file to copy shortcut

---
+++ Add to desktop.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+cls
+ECHO *************************************
+ECHO *************************************
+ECHO **                                 **
+ECHO **      Creating Shortcut          **
+ECHO **                                 **
+ECHO *************************************
+ECHO -------------------------------------
+copy "Confirm POs.lnk" %userprofile%\Desktop
+ECHO -------------------------------------
+ECHO.
+ECHO.
+ECHO.
+ECHO Press any key to quit..
+pause > nul
